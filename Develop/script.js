ed6cd5d8bd9cb5fae7d8a9278ad854cb595a25c1
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-02-11 20:08:22 -0500

    modified arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,68 +1,18 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var lengthOpt = document.querySelector("#length");
-var upperOpt = document.querySelector("#uppercase");
-var lowerOpt = document.querySelector("#lowercase");
-var numbersOpt = document.querySelector("#numbers");
-var synbolOpt = document.querySelector("#symbols");
+let addUpper = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 
-// Generate functions
 
-let randomFunctions = {
-  upper: addNumber,
-  lower: addLower,
-  number:addNumber,
-  symbol: addSymbol
-};
+let addLower = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 
-  //26 letters in alphabet. charcode uppercase # is 65 - 90
 
-  function addUpper(){
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+let addNumber = String.fromCharCode(Math.floor(Math.random()* 10) + 48);
+
+
+let addSymbols = ['!','@','#','$','%','^','&','*','(',')','_','+']; 
+
+
+function getPasswordOptions() {
+  // Variable to store length of password from user input
+  var length = parseInt(
+    prompt('How many characters would you like your password to contain?')
+  );
   }
-  console.log(addUpper());
-    //charcode lowercase # 97 - 122
-  
-  function addLower(){
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-  }
-  console.log(addLower());
-  
-    // charcode for number 48 - 57
-  
-  function addNumber(){
-    return String.fromCharCode(Math.floor(Math.random()* 10) + 48);
-  }
-  console.log(addNumber());
-  
-    //use string from symbols on top row "~!@#$%^&*()_+"
-  
-  function addSymbol(){
-    let symbols = "!@#$%^&*()_+";
-    return symbols[Math.floor(Math.random() * symbols.length)];
-  }
-  console.log(addSymbol());
-
-  // prompt user for options
-function askOptions(){
-  let length = parseInt (
-    prompt('How many characters should your password be?'));
-  
-  if(length < 8) {
-    alert('Password length must be at least 8 characters long');
-    return;
-  }
-  
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-console.log(writePassword);
-}
-
-  // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
