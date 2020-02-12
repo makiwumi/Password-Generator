a4619989b53968513ff35a15519bbab90e0dd9c6
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-02-12 16:39:27 -0500

    adding char codes

--- Develop/script.js
+++ Develop/script.js
@@ -10,9 +10,23 @@ let addNumber = String.fromCharCode(Math.floor(Math.random()* 10) + 48);
 let addSymbols = ['!','@','#','$','%','^','&','*','(',')','_','+']; 
 
 
-function getPasswordOptions() {
-  // Variable to store length of password from user input
-  var length = parseInt(
-    prompt('How many characters would you like your password to contain?')
-  );
+let length = prompt('How many character would you like your password to be?');
+
+
+  // Conditional statement to check if password length is a number. Prompts end if this evaluates false
+  if (length === true) {
+    alert('Password length must be provided as a number');
+    return;
+  }
+
+  // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
+  if (length < 8) {
+    alert('Password length must be at least 8 characters');
+    return;
+  }
+
+  // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
+  if (length > 128) {
+    alert('Password length must less than 129 characters');
+    return;
   }
